6ce630a630d9d1f96be0b894208ff02ea3eed486
Chris Reiner <cjreiner@yahoo.com>
2015-01-27 17:31:37 -0500

    add user functionality

--- class_example_from_Codecademy.rb
+++ class_example_from_Codecademy.rb
@@ -3,7 +3,6 @@ class Computer
    @@users = {}
    
    def initialize(username,password)
-       
       @username = username
       @password = password
       @files = {}
@@ -16,10 +15,20 @@ class Computer
         puts "New files created at: #{time}"
     end
     
-    def Computer.get_users()
+    def get_users()
         @@users
     end
     
+    def add_user(username,password)
+        @@users[username] = password
+    end
 end
 
 my_computer = Computer.new("Chris",12345)
+my_computer.add_user("Rhys",99999)
+
+my_computer.create("text")
+
+puts my_computer.get_users
+
+my_computer.get_users.each {|k,v| puts v}
